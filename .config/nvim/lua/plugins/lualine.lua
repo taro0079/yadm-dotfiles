651e7bfe766c16c106cfc1236081bb435fd15807
Taro Morita <morita0079@gmail.com>
2023-08-28 12:35:53 +0900

    show relative path of current buffer file

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,32 +1,37 @@
 local lualine = require('lualine')
 lualine.setup {
-  options = {
-    theme = "auto",
-    section_separators = { left = '', right = '' },
-    component_separators = '|',
-
-  },
-  sections = {
-    lualine_a = {
-      {
-        'mode',
-        icons_enabled = true,
-        separator = { left = '' },
-        right_padding = 2,
-        path = 1
-
-      },
+    options = {
+        theme = "auto",
+        section_separators = { left = '', right = '' },
+        component_separators = '|',
 
     },
-    lualine_b = {
-      {
-        'branch',
-        icon = { '' },
-      },
-      'diff'
-    },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
-    },
-  }
+    sections = {
+        lualine_a = {
+            {
+                'mode',
+                icons_enabled = true,
+                separator = { left = '' },
+                right_padding = 2,
+
+            },
+
+        },
+        lualine_b = {
+            {
+                'filename',
+                path = 1
+            },
+        },
+        lualine_c = {
+            {
+                'branch',
+                icon = { '' },
+            },
+            'diff'
+        },
+        lualine_z = {
+            { 'location', separator = { right = '' }, left_padding = 2 },
+        },
+    }
 }
