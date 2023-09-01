f7ebaedc6d1976731761182f57801774d275d142
Taro Morita <morita0079@gmail.com>
2023-09-01 16:38:51 +0900

    add japanese font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,55 +3,57 @@ local mac = wezterm.target_triple:find("darwin")
 local linux = wezterm.target_triple:find("linux")
 
 local mykeys = {
-  { key = 'n', mods = 'OPT',  action = wezterm.action.ToggleFullScreen },
-  { key = '8', mods = 'CTRL', action = wezterm.action.PaneSelect },
-  { key = "v", mods = "OPT",  action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-  { key = "s", mods = "OPT",  action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) }
+    { key = 'n', mods = 'OPT',  action = wezterm.action.ToggleFullScreen },
+    { key = '8', mods = 'CTRL', action = wezterm.action.PaneSelect },
+    { key = "v", mods = "OPT",  action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+    { key = "s", mods = "OPT",  action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) }
 }
 
 -- swicth tabs
 for i = 1, 8 do
-  table.insert(mykeys, {
-    key = tostring(i),
-    mods = "OPT",
-    action = wezterm.action { ActivateTab = i - 1 }
-  })
+    table.insert(mykeys, {
+        key = tostring(i),
+        mods = "OPT",
+        action = wezterm.action { ActivateTab = i - 1 }
+    })
 end
 
 if mac then
-  return {
-    font = wezterm.font_with_fallback {
-      { family = 'UbuntuMono Nerd Font', weight = "Regular", italic = false },
-      { family = 'UbuntuMono Nerd Font', weight = "Regular", italic = true },
-      { family = 'UbuntuMono Nerd Font', weight = "Bold" }
-    },
-   -- disable_default_key_bindings = true,
-    font_size = 18,
-    keys = mykeys,
-    -- color_scheme = "Tokyo Night Storm",
-    send_composed_key_when_right_alt_is_pressed = false,
-    hide_tab_bar_if_only_one_tab = true,
-    window_padding = {
-      left = 0,
-      right = 0,
-      top = 0,
-      bottom = 0
-    },
-    adjust_window_size_when_changing_font_size = false,
-    front_end = "WebGpu",
-  }
+    return {
+        font = wezterm.font_with_fallback {
+            { family = 'UbuntuMono Nerd Font', weight = "Regular", italic = false },
+            { family = 'UbuntuMono Nerd Font', weight = "Regular", italic = true },
+            { family = 'UbuntuMono Nerd Font', weight = "Bold" },
+            { family = 'HackGen Console NF', weight = "Bold" }
+
+        },
+        -- disable_default_key_bindings = true,
+        font_size = 18,
+        keys = mykeys,
+        -- color_scheme = "Tokyo Night Storm",
+        send_composed_key_when_right_alt_is_pressed = false,
+        hide_tab_bar_if_only_one_tab = true,
+        window_padding = {
+            left = 0,
+            right = 0,
+            top = 0,
+            bottom = 0
+        },
+        adjust_window_size_when_changing_font_size = false,
+        front_end = "WebGpu",
+    }
 elseif linux then
-  return {
-    color_scheme = "tokyonight",
-    enable_tab_bar = false,
-   font = wezterm.font('FiraCode Nerd Font', { weight = 'Medium', italic = false }),
-    font_size = 10,
-    color_scheme = 'tokyonight',
-    window_padding = {
-      left = 0,
-      right = 0,
-      top = 0,
-      bottom = 0
-    },
-  }
+    return {
+        color_scheme = "tokyonight",
+        enable_tab_bar = false,
+        font = wezterm.font('FiraCode Nerd Font', { weight = 'Medium', italic = false }),
+        font_size = 10,
+        color_scheme = 'tokyonight',
+        window_padding = {
+            left = 0,
+            right = 0,
+            top = 0,
+            bottom = 0
+        },
+    }
 end
