f76504eeacf76968585e2df8aadece1bf0ba3f44
Taro Morita <morita0079@gmail.com>
2023-11-12 15:51:53 +0900

    auto php-cs-fixer on save

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,3 +4,12 @@ require('keys')
 require('config')
 require('gui-settings')
 require('helper.matchit-setting')
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+    pattern = "*php",
+    callback = function()
+        vim.cmd('!php-cs-fixer fix %')
+        vim.cmd('e!')
+
+    end
+})
