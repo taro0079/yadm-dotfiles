42b8ec6f81f668719f9db844d8caaffe5c8750ec
Taro Morita <morita0079@gmail.com>
2023-11-20 15:52:02 +0900

    add fix for error of treesitter-context when in command line window

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,3 +12,20 @@ vim.api.nvim_create_autocmd('BufWritePost', {
         vim.cmd('e!')
     end
 })
+
+incremental_selection = {
+  enable = true,
+  keymaps = {
+    init_selection = "<CR>",
+    node_incremental = "<CR>",
+    node_decremental = "<BS>",
+    scope_incremental = "<TAB>",
+  },
+  is_supported = function ()
+    local mode = vim.api.nvim_get_mode().mode
+    if mode == "c" then 
+      return false
+    end
+    return true
+  end
+}
