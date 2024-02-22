1acf8b083e7b4963570a9ca6f6c77625354af511
Morita Taro <38848223+taro0079@users.noreply.github.com>
2024-02-22 15:09:54 +0900

    change config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ if status is-interactive
     fzf_key_bindings
     zoxide init fish | source
     set -x PATH $PATH $HOME/.config/composer/vendor/bin
+    set -gx PATH "$HOME/.cargo/bin" $PATH;
 
 end
 function fbr
@@ -11,3 +12,31 @@ function fbr
     set branch (echo "$branches" | fzf-tmux -d (math 2 + (count $branches)) +m)
     git checkout (echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 end
+
+function fshow
+    git log --graph --color=always \
+        --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" $argv | 
+    fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+        --bind "ctrl-m:execute:
+                  (grep -o '[a-f0-9]\\{7\\}' | head -1 |
+                  xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                  {}
+FZF-EOF"
+end
+function dox
+    docker exec -it (docker ps --format "{{.Names}}" | fzf) sh
+end
+alias e='exa -la --icons --git'
+alias ls=e
+
+# alias for git command
+alias gst='git status'
+alias gsw='git switch'
+alias gc='git commit'
+alias gpush='git push origin'
+alias gpull='git pull origin'
+alias gcb='git checkout -b'
+alias gco='git checkout'
+alias gaa='git add .'
+alias ga='git add'
+
