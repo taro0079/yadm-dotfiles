1af2c999b0651898dadc71db7993cb0307cfa422
Taro Morita <morita0079@gmail.com>
2024-05-20 19:00:03 +0900

    change vimrc

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -475,19 +475,6 @@ require("lazy").setup({
     --     end
     -- },
     {
-        "gbprod/phpactor.nvim",
-        build = function()
-            require("phpactor.handler.update")()
-        end,
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "neovim/nvim-lspconfig"
-        },
-        opts = {
-            -- you're options coes here
-        },
-    },
-    {
         'tpope/vim-projectionist',
         config = function()
             vim.g.projectionist_heuristics = {
