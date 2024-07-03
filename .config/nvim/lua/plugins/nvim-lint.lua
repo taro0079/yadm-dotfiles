dd79c7cd06519a33d423ebbf4bf09939707bdd6c
Taro Morita <morita0079@gmail.com>
2024-07-03 12:09:20 +0900

    change linter config

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -11,6 +11,19 @@ phpmd.args = {
 
 }
 
+local phpstan = require('lint').linters.phpstan
+phpstan.args = {
+    'analyse',
+    '--error-format',
+    'json',
+    '--no-progress',
+    '--memory-limit',
+    '1G',
+    '--configuration',
+    './phpstan.dist.neon',
+    '-'
+}
+
 
 local lint_autogroup = vim.api.nvim_create_augroup("lint", { clear = false }) -- clear falseにしないとnvim-lintのプロセスが終了されずにlinterのprocessがスタックしていく
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" },
