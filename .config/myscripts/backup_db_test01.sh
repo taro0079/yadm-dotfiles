c1d752ef4b683fea968068bcc95ce8f163224de5
Taro Morita <morita0079@gmail.com>
2024-07-08 14:24:39 +0900

    add dump script

---
+++ .config/myscripts/backup_db_test01.sh
@@ -0,0 +1,10 @@
+HOST="172.27.250.17"
+PORT="3306"
+USER="root"
+PASSWORD="!ChangeMe!"
+
+DATABASE="app_db"
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+DUMP_FILE_NAME="dump_app_db_at_$TIMESTAMP.sql"
+
+mysqldump -h $HOST -P $PORT -u $USER -p$PASSWORD $DATABASE > $DUMP_FILE_NAME
