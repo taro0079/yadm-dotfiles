dbfd8c0acab5e7f8bc73bece48ef0a2820184be4
Taro Morita <morita0079@gmail.com>
2024-07-10 12:02:13 +0900

    database backup script

--- .config/myscripts/backup_db_test01.sh
+++ .config/myscripts/backup_db_test01.sh
@@ -7,4 +7,7 @@ DATABASE="app_db"
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 DUMP_FILE_NAME="dump_app_db_at_$TIMESTAMP.sql"
 
+LOCALHOST="127.0.0.1"
+
 mysqldump -h $HOST -P $PORT -u $USER -p$PASSWORD $DATABASE > $DUMP_FILE_NAME
+mysql -h $LOCALHOST -P $PORT -u $USER -p$PASSWORD $DATABASE < $DUMP_FILE_NAME
