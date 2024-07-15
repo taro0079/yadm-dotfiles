1d7edc444f27aa52d6ad9e08b6eaa0c75f1e070e
Morita Taro <38848223+taro0079@users.noreply.github.com>
2024-07-15 23:57:17 +0900

    some change

---
+++ .config/nvim/lua/helper/file_exist.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.file_exist = function(path)
+    local f = io.open(path, "r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    else
+        return false
+    end
+end
+
+return M
