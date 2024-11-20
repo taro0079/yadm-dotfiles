822b72ab78c157793125c0394be0a496c3885e8a
Taro Morita <morita0079@gmail.com>
2024-11-20 15:21:04 +0900

    fix tmux sessionizer

--- .local/scripts/tmux-sessionizer.sh
+++ .local/scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/dev -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/dev /var/lib/rpst-api-docker /var/lib/rpst-api-docker/api /var/lib/rpst-api-docker/rpst-v2 -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
