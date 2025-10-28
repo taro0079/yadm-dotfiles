01ba84ecf7796596e6bffc3ca425e68ecbd3ef47
taro_morita <taro_morita@rpst.co.jp>
2025-10-28 13:47:52 +0900

    add flash.nvim alnative to vim-sneak

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -346,13 +346,17 @@ require("lazy").setup({
         dependencies = "nvim-treesitter/nvim-treesitter",
     },
     {
-        'justinmk/vim-sneak',
-        event = "BufEnter",
-        config = function()
-            vim.cmd [[
-            let g:sneak#label = 1
-            ]]
-        end
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        keys = {
+            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        },
     },
     {
         'tpope/vim-surround',
