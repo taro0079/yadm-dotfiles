96b1f5c37e2a457de16ca385029bf9cd8a80f5a5
Taro Morita <morita0079@gmail.com>
2026-01-05 15:54:44 +0900

    fix rpst-php-unit

--- .local/scripts/rpst-php-unit.sh
+++ .local/scripts/rpst-php-unit.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # set -euo pipefail
-filename=${1:?Usage, $0 <relative_test_file>}
+if [[ $# -ge 1 ]]; then
+  filename="$1"
+else
+ IFS= read -r filename
+ : "${filename:?Usage: $0 <relative_test_file>}"
+fi
 remote_host="dev-tmorita"
 user="taro_morita"
 remote_root="/var/www/rpst-v2/dev"
 phpunit="$remote_root/vendor/bin/phpunit"
 config="$remote_root/tests/app/phpunit/v9/phpunit.xml.dist"
 test_target_path=$(printf "/var/www/rpst-v2/dev/%s" "$filename")
-ssh -q "${user}@${remote_host}" -- php "$phpunit" -c "$config" "$test_target_path" > /tmp/rpst-php-unit 2>&1
+ssh -q "${user}@${remote_host}" -- php "$phpunit" -c "$config" "$test_target_path"
 # cat /tmp/rpst-php-unit
